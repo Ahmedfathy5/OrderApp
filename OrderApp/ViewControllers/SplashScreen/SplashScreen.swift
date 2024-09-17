f3546bc4f46eafd090cc1a2dff533b61beeed6f6
Lord Ahmed Fathy <ahmedfathyios1@gmail.com>
2024-09-17 21:35:22 +0300

    add Splash Screen

---
+++ OrderApp/ViewControllers/SplashScreen/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  OrderApp
+//
+//  Created by Ahmed Fathi on 17/09/2024.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
